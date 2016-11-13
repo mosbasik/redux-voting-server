0b916519c5494c91f1f6014393480618877e2beb
Peter Henry <me@peterhenry.net>
2016-11-13 09:54:31 -0600

    completed ending the vote

--- src/core.js
+++ src/core.js
@@ -16,6 +16,11 @@ function getWinners(vote) {
 
 export function next(state) {
     const entries = state.get('entries').concat(getWinners(state.get('vote')));
+    if (entries.size === 1) {
+        return state.remove('vote')
+                    .remove('entries')
+                    .set('winner', entries.first());
+    }
     return state.merge({
         vote: Map({
             pair: entries.take(2)
